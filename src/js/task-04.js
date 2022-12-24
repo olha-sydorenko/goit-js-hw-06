6bbdc20a983e91d696fbb665bde052e5edcedf4e
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2022-12-24 16:37:29 +0200

    task-04

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,1 +1,16 @@
 let counterValue = 0;
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const counter = document.querySelector('#value');
+
+decrementBtn.addEventListener('click', onDecrementClick);
+function onDecrementClick(event) {
+  counterValue -= 1;
+  value.textContent = counterValue;
+}
+
+incrementBtn.addEventListener('click', onIncrementClick);
+function onIncrementClick(event) {
+  counterValue += 1;
+  value.textContent = counterValue;
+}
