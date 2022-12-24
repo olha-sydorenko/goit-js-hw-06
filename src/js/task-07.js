1c4f497836f87e031a260a90cb451f5e0d0c6c11
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2022-12-24 21:50:29 +0200

    Update task-07.js

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,6 +1,6 @@
 const slider = document.querySelector('#font-size-control');
 const text = document.querySelector('#text');
-//slider.step = '10';
+
 
 slider.addEventListener('change', event => {
   text.style.fontSize = event.currentTarget.value + 'px';
