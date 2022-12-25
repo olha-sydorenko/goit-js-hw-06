31277a86b70253e2f3c41c69824e2a273a0b1702
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2022-12-25 10:24:30 +0200

    task-09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const changeColorBtn = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', changeBodyColor);
+function changeBodyColor(event) {
+  document.body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+  // console.log(event.currentTarget.value);
+  // console.log(getRandomHexColor());
+}
