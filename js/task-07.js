ad2c0bc70b13845aeef1f2e7c3c79526038c6af3
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2022-12-25 23:41:35 +0200

    ..

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,6 @@
 const slider = document.querySelector('#font-size-control');
 const text = document.querySelector('#text');
 
-
 slider.addEventListener('change', event => {
   text.style.fontSize = event.currentTarget.value + 'px';
 });
