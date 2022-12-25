240fb316fdbb9d818b3e1d2244f1445b109d33c7
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2022-12-25 15:55:08 +0200

    task 09 updated

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -5,9 +5,9 @@ const changeColorBtn = document.querySelector('.change-color');
 const color = document.querySelector('.color');
 
 changeColorBtn.addEventListener('click', changeBodyColor);
+
 function changeBodyColor(event) {
-  document.body.style.backgroundColor = getRandomHexColor();
-  color.textContent = getRandomHexColor();
-  // console.log(event.currentTarget.value);
-  // console.log(getRandomHexColor());
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  color.textContent = randomColor;
 }
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,29 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+const boxes = document.querySelector('#boxes');
+boxes.style = 'padding-top: 10px; display: flex; gap: 10px;';
+
+const box = document.createElement('div');
+box.classList.add('.box');
+box.style = 'width: 30px; height: 30px; background-color: red;';
+
+const controls = document.querySelector('#controls');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+let quantity;
+
+createBtn.addEventListener('click', oncreateBtnClick);
+
+controls.firstElementChild.addEventListener('input', event => {
+  quantity = event.currentTarget.value;
+});
+
+function oncreateBtnClick(event) {
+  for (let i = 0; i <= quantity; i += 1) {
+    boxes.append(box);
+    console.log(quantity);
+  }
 }
+//console.log('hello');
