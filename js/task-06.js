6672cb097c3a3d5fefbe954f24439cb66243a48c
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2022-12-26 15:40:47 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,7 @@
 const textInput = document.querySelector('#validation-input');
 
 textInput.addEventListener('blur', () => {
-  if (textInput.value.length === parseInt(textInput.dataset.length)) {
+  if (textInput.value.length === Number(textInput.dataset.length)) {
     textInput.classList.remove('invalid');
     textInput.classList.add('valid');
   } else {
